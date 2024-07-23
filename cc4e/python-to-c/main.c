888b918a82e17cf6bd09fb13d9dce4ffe04ae13b
Zach Holmes <zholmes430@gmail.com>
2024-07-23 21:18:56 +1000

    clean up

---
+++ cc4e/python-to-c/main.c
@@ -0,0 +1,11 @@
+#import <stdio.h>
+
+// print('Hello world')
+// print('Answer', 17)
+// print('Name', 'Sarah')
+
+int main(){
+	printf("hello");
+	printf("Answer", 17);
+	printf("hello" + "4");
+};
