8b039e188c5137cadcab40b16a2501b53fae9b14
Zach Holmes <zholmes430@gmail.com>
2024-07-24 18:44:41 +1000

    complete rs3

--- cc4e/python-to-c/rs-3.c
+++ cc4e/python-to-c/rs-3.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+
+// print('Enter name')
+// name = input()
+// print('Hello', name)
+
 int main() {
     char name[101];
     printf("Enter name\n");
-    scanf("s%", name);
+    scanf("%s", name);
+    printf("Hello %s\n", name);
 
     return 0;
 }
