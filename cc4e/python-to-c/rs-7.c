da89f61aabf9d7e52abe2150fbaee64adff404b3
Zach Holmes <zholmes430@gmail.com>
2024-07-27 15:24:58 +1000

    working rs-7, funky cause they want old C

--- cc4e/python-to-c/rs-7.c
+++ cc4e/python-to-c/rs-7.c
@@ -1,8 +1,3 @@
-// #include <cstdio>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
 /*
 Exercise RS-7: Write a C program to determine the minimum and maximum of a sequence of integer numbers on input, 
 terminated by the string "done". The patterns for input and output should match the following Python application. 
@@ -25,18 +20,42 @@ print('Maximum', maxval)
 print('Minimum', minval)
 */
 
+// #include <cstdio>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
 int main() {
-	int minVal;
-	int maxVal;
-    // char input[1001];
+    int maxVal = INT_MIN; // Initialize to the smallest possible integer
+    int minVal = INT_MAX; // Initialize to the largest possible integer
     char line[1001];
     int inputNum;
 	
-    while (1) {    
-        // take input for int 
-        // gets(line);
-        fgets(line, 1000, stdin);
-        // strip whitespace ?
+    // while (1) {    
+    //     // take input for int 
+    //     gets(line);
+    //     // fgets(line, 1000, stdin);
+    //     // strip whitespace ?
+    //     // Strip trailing newline character
+    //     line[strcspn(line, "\n")] = '\0';
+    //     // check if done 
+    //     if ( strcmp(line, "done") == 0 ) {
+    //         break;
+    //     }
+    //     // convert to int
+    //     inputNum = atoi(line);
+    //     // check for maxval 
+    //     if (inputNum > maxVal) {
+    //         maxVal = inputNum;
+    //     }
+    //     // check for minval
+    //     if (inputNum < minVal) {
+    //         minVal = inputNum;
+    //     }
+	// }
+
+    while (gets(line) != NULL) {    
         // Strip trailing newline character
         line[strcspn(line, "\n")] = '\0';
         // check if done 
@@ -46,11 +65,11 @@ int main() {
         // convert to int
         inputNum = atoi(line);
         // check for maxval 
-        if (inputNum > maxVal) {
+        if (inputNum > maxVal || maxVal == 0 ) {
             maxVal = inputNum;
         }
         // check for minval
-        if (inputNum < minVal) {
+        if (inputNum < minVal || minVal == 0) {
             minVal = inputNum;
         }
 	}
