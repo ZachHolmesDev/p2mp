01af6557387c704ab1694a1e711a500235f838ff
Zach Holmes <zholmes430@gmail.com>
2024-07-28 10:23:40 +1000

    add lessons

---
+++ cc4e/3.introduction/ex-04-fixSyntaxErr.c
@@ -0,0 +1,20 @@
+/* Assignment 4 LBS 290 - Fall 1991 - syntax errors */
+//Fun Fact:Dr. Chuck used this exact assignment while teaching C - in LBS 290 - Fall 1991.
+#include "stdio.h"
+
+main () {
+  char c;
+  int 1value;
+  int i;
+  flat x;
+
+  printf("Hello there and welcome to the program\n);
+  i = 10;
+  x = 2.50;
+  x = x + 1.L;
+  1value = 15;
+/* This is a comment
+  printf("This statement should print out, why doesn't it?\n");
+/* And another comment */
+  printf("This statement does print out, yay!\n");
+}
