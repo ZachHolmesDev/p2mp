b5c0545b915ea979c79dcd7b349cdc7d04837f74
Zach Holmes <zach.holmes@canstar.com.au>
2024-08-08 07:55:26 +1000

    work on 1-10

--- cc4e/3.introduction/1-10.c
+++ cc4e/3.introduction/1-10.c
@@ -4,9 +4,13 @@ one per line. A word is any string separated by a space or newline.
 */
 
 #include <stdio.h>
+#include <string.h>
 int main() {
     int c;
     while ((c = getchar()) != EOF) {
+        
+        
+        if (strcmp(c, " "))
         putchar(c);
     }
 }
