d090740cd4c5139c959e12c49bef2efe8950b9a0
Zach Holmes <zholmes430@gmail.com>
2024-08-11 12:13:56 +1000

    1-10 done

--- cc4e/3.introduction/1-10.c
+++ cc4e/3.introduction/1-10.c
@@ -1,18 +1,23 @@
 /*
-K&R Exercise 1-10. Write a program which prints the words in its input, 
+K&R Exercise 1-10. Write a program which prints the words in its input,
 one per line. A word is any string separated by a space or newline.
 */
 
 #include <stdio.h>
-#include <string.h>
+// #include <string.h>
 int main() {
-    int c;
-    while ((c = getchar()) != EOF) {
-        
-        
-        if (strcmp(c, " "))
-        putchar(c);
-    }
+	// input
+	int c;
+	char word[101];
+
+	while ((c = getchar()) != EOF) {
+
+		if (c == ' ' || c == '\n') {
+			putchar('\n');
+			continue;
+		}
+		putchar(c);
+	}
 }
 
 /*
